8f073c9ef6a842fa3e40847cf9f51c463725bda8
OlgaSvishch <olga.svishch7@gmail.com>
2019-02-16 15:05:59 +0300

    init commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	CofeFactory cofeFactory = new CofeFactory();
+
+	Cofe americano = cofeFactory.getCofe(CofeTipes.AMERICANO);
+	Cofe ecsprosso = cofeFactory.getCofe(CofeTipes.ECSPRESSO);
+	Cofe late = cofeFactory.getCofe(CofeTipes.LATE);
+
+	americano.getCofe();
+	ecsprosso.getCofe();
+	late.getCofe();
     }
 }
